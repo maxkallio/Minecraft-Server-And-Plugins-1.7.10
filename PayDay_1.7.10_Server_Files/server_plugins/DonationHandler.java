01b50dabdd26beb4ef6bcdc33149a9f87c3baf04
maxkallio <122458059+maxkallio@users.noreply.github.com>
2025-03-05 12:24:27 +0100

    Update DonationHandler.java

--- PayDay_1.7.10_Server_Files/server_plugins/DonationHandler.java
+++ PayDay_1.7.10_Server_Files/server_plugins/DonationHandler.java
@@ -1,9 +1,28 @@
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 
-package com.payday.server.donations;
+public class DonationHandler extends JavaPlugin {
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("donate")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                if (args.length < 2) {
+                    player.sendMessage(ChatColor.RED + "Usage: /donate <amount> <rank>");
+                    return true;
+                }
+                double amount = Double.parseDouble(args[0]);
+                String rank = args[1];
 
-public class DonationHandler {
-    public static void processDonation(String playerName, String rank) {
-        System.out.println("Processing donation for: " + playerName + " - Rank: " + rank);
-        // Implement rank assignment logic here
+                Bukkit.dispatchCommand(Bukkit.getConsoleSender(), "pex user " + player.getName() + " group set " + rank);
+                player.sendMessage(ChatColor.GREEN + "Thank you for donating! You are now " + rank);
+            }
+            return true;
+        }
+        return false;
     }
 }
