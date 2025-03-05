3d3ab40ded78a5077b69652a3c4d82059e7a49af
maxkallio <122458059+maxkallio@users.noreply.github.com>
2025-03-05 12:24:02 +0100

    Update MainPlugin.java

--- PayDay_1.7.10_Server_Files/server_plugins/MainPlugin.java
+++ PayDay_1.7.10_Server_Files/server_plugins/MainPlugin.java
@@ -1,16 +1,30 @@
-
-package com.payday.server;
-
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class MainPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("PayDay 1.7.10 Server Plugin Enabled!");
+        getLogger().info("[PayDayPlugin] Enabled for 1.7.10 server!");
     }
 
     @Override
     public void onDisable() {
-        getLogger().info("PayDay 1.7.10 Server Plugin Disabled!");
+        getLogger().info("[PayDayPlugin] Plugin disabled.");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("rank")) {
+            if (sender instanceof Player) {
+                Player player = (Player) sender;
+                player.sendMessage(ChatColor.GOLD + "Your current rank: " + ChatColor.AQUA + "Player");
+            }
+            return true;
+        }
+        return false;
     }
 }
