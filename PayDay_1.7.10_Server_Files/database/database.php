e6191393e0018e610f0a5ac8c3a3d52d1c26b6d9
maxkallio <122458059+maxkallio@users.noreply.github.com>
2025-03-05 16:32:23 +0100

    database.php

---
+++ PayDay_1.7.10_Server_Files/database/database.php
@@ -0,0 +1,10 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "password";
+$db = "payday_server";
+$conn = new mysqli($host, $user, $pass, $db);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
